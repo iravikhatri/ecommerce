fea2ac78c84806f46ff1ab5ef1f9f2dc37c54b63
Ravi Kumar <43924877+iravikumar@users.noreply.github.com>
2019-09-21 15:59:52 +0530

    Update hide_fields.js

--- theme_livila/static/src/hide_fields.js
+++ theme_livila/static/src/hide_fields.js
@@ -1,3 +1,5 @@
+alert('hello')
+
 $("table.o_account_reports_table th:nth-child('3')").hide();
 $("table.o_account_reports_table td:nth-child('3')").hide();
 $("table.o_account_reports_table th:nth-child('4')").hide();
