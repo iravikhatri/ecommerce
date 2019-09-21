21304bccd0a3305af804b3b2f6a5a67e9198f813
Ravi Kumar <43924877+iravikumar@users.noreply.github.com>
2019-09-21 15:48:14 +0530

    some

---
+++ theme_livila/static/src/hide_fields.js
@@ -0,0 +1,4 @@
+$("table.o_account_reports_table th:nth-child('3')").hide();
+$("table.o_account_reports_table td:nth-child('3')").hide();
+$("table.o_account_reports_table th:nth-child('4')").hide();
+$("table.o_account_reports_table td:nth-child('4')").hide();
